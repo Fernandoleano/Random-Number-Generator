339b5b235c502f4e388008478c48a5e4579d8541
Fernando Leano <68082556+Fernandoleano@users.noreply.github.com>
2021-05-26 14:41:27 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// var random = Math.floor(Math.random() * 10 ) + 1;
+// console.log(random);
+
+function RandomNumber() {
+    var random = Math.floor(Math.random() * 10 ) + 1;
+    document.getElementById("display").innerHTML = random;
+}
